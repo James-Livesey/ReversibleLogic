b2105f4fffe50f84cc21b4cf1d9218fbff7155d3
James-Livesey <jamesliveg@gmail.com>
2021-03-16 21:16:37 +0000

    Add basic code for full adder reversal

--- examples/fulladder.py
+++ examples/fulladder.py
@@ -10,8 +10,30 @@ S = RXOR(RXOR(Input("A"), Input("B")), Input("Cin"))
 Cout = ROR(RAND(Input("Cin"), RXOR(Input("A"), Input("B"))), RAND(Input("A"), Input("B")))
 
 permsS = solve(S, False)
-permsCout = solve(Cout, True)
+permsCout = solve(Cout, False)
 
 print("S:", permsS)
 print("Cout:", permsCout)
 print("Intersect:", intersect(permsS, permsCout))
+
+################################################################################
+
+# output = [True, False, True, False]
+
+# def fullAdderFactory(id, lastCin):
+#     return {
+#         "S": RXOR(RXOR(Input("A" + id), Input("B" + id)), lastCin),
+#         "Cout": ROR(RAND(lastCin, RXOR(Input("A" + id), Input("B" + id))), RAND(Input("A" + id), Input("B" + id)))
+#     }
+
+# S0 = fullAdderFactory("0", Input("Cin0"))
+# S1 = fullAdderFactory("1", S0["Cout"])
+# S2 = fullAdderFactory("2", S1["Cout"])
+# S3 = fullAdderFactory("3", S2["Cout"])
+
+# permsS0 = solve(S0["S"], output[0])
+# permsS1 = solve(S1["S"], output[1])
+# permsS2 = solve(S2["S"], output[2])
+# permsS3 = solve(S3["S"], output[3])
+
+# print(permsS0, permsS1, permsS2, permsS3)
